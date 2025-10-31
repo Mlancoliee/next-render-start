8a077cb948b43b137ad74f6b428a4315b72aab5f
jesperxu <jesperxu@tencent.com>
2025-10-31 13:33:12 +0800

    , error

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,5 +2,9 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
     // 自定义状态码
+    const o = {
+        message: 'Hello World'
+        status: 502
+    }
     return NextResponse.json({ message: 'Hello World' }, { status: 502 })
 }

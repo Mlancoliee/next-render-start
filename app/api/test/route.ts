75fe3959b342a6d5a27f8fcc31f2c9f696bcdafb
jesperxu <jesperxu@tencent.com>
2025-10-31 13:26:38 +0800

    主动返回502

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
     // 自定义状态码
-    return NextResponse.json({ message: 'Hello World' }, { status: 404 })
+    return NextResponse.json({ message: 'Hello World' }, { status: 502 })
 }

4d169229f3a0cb0957f4a2adbd901699eb7e56ca
jesperxu <jesperxu@tencent.com>
2025-10-31 14:47:37 +0800

    403

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-    // 返回错误
-    return NextResponse.error()
+    // 自定义状态码
+
+    return NextResponse.json({ message: 'Hello World' }, { status: 403 })
 }

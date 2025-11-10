b3c33af3478fde904a70f9a902099ce5ba736f47
jesperxu <jesperxu@tencent.com>
2025-11-10 15:10:52 +0800

    next api and middleware

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
     // 自定义状态码
-
+    // 调用api/test1
+    const res = await fetch('http://localhost:3000/api/test1');
+    const data = await res.json();
     return NextResponse.json({ message: 'Hello World' }, { status: 403 })
 }
--- app/api/test1/route.ts
+++ app/api/test1/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from 'next/server'
 export async function GET(request: Request) {
     // 自定义状态码
     console.log('Received request:', request);
-    // return NextResponse.json({ message: 'Hello World' }, { status: 403 })
+    return NextResponse.json({ message: 'Hello test1' }, { status: 403 })
 }

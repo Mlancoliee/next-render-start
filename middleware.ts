05b83250620502a77b20b5bba6dbb447e0ad9ac8
jesperxu <jesperxu@tencent.com>
2025-11-11 14:23:03 +0800

    add middleware,delete node runtime

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 //   runtime: 'nodejs', // optional: use 'nodejs' or omit for 'edge' (default)
 // };
  
-// export default function middleware(request: Request) {
-//   console.log('Request to:', request.url);
-//   // return new Response('Logging request URL from Middleware');
-// }
+export default function middleware(request: Request) {
+  console.log('Request to:', request.url);
+  // return new Response('Logging request URL from Middleware');
+}
